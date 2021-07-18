b5d2e515f2e4ead2428053f9924c09760c21ccaf
ThiagoPanini <thipanini94@gmail.com>
2021-07-18 11:23:41 -0300

    Script responsavel por requisitar todas as imagens de jogadores obtidas pelo site da NBA

--- scripts/all_players_images.py
+++ scripts/all_players_images.py
@@ -32,9 +32,16 @@ Table of Contents
 
 # Funções para extração de imagens de jogadores
 from core.images import get_players_images
+import os
+import pandas as pd
 
 # Definindo parâmetros de consulta e de filtros
 STATIC_IMG_URL = 'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/team_id/2020/260x190/player_id'
+PLAYER_REF = 'player_id'
+PROJECT_PATH = os.getcwd()
+TEMPLATE_IMG_PATH = os.path.join(PROJECT_PATH, 'data/unknown_player_template.png')
+GAMELOG_PATH = os.path.join(PROJECT_PATH, 'data/all_players_gamelog.csv')
+IMGS_PATH = os.path.join(PROJECT_PATH, 'data/images/')
 
 
 """
@@ -47,4 +54,25 @@ os jogadores ativos da liga.
 """
 
 # Salvando imagens de jogadores
-success_requests, fail_requests = get_players_images(static_url=STATIC_IMG_URL)
+success_requests, fail_requests = get_players_images(static_url=STATIC_IMG_URL, 
+                                                     imgs_path=IMGS_PATH,
+                                                     player_name_code_col=PLAYER_REF,
+                                                     template_img_path=TEMPLATE_IMG_PATH)
+
+# Deletando imagens baixadas de jogadores não presentes na base
+gamelog = pd.read_csv(GAMELOG_PATH)
+players = sorted(gamelog[PLAYER_REF].unique())
+players_imgs = os.listdir(IMGS_PATH)
+
+print(f'Jogadores únicos na base de gamelog: {len(players)}')
+print(f'Imagens totais baixadas: {len(players_imgs)}')
+
+# Iterando sobre as imagens baixadas
+for img in players_imgs:
+    img_split = img[:-4]
+    if int(img_split) not in players:
+        # Removendo imagem caso jogador inexistente na base
+        os.remove(os.path.join(IMGS_PATH, img))
+        
+# Verificando números finais
+print(f'Total de imagens após a remoção: {len(os.listdir(IMGS_PATH))}')
