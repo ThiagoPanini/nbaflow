29dc06eb1b717c83413aa40a7cce2e97eae2463f
Thiago Panini <panini.development@gmail.com>
2023-09-11 19:00:03 -0300

    docs: included docstring for all modules

--- nbaflow/players.py
+++ nbaflow/players.py
@@ -9,20 +9,14 @@ ___
 
 # Importing libraries
 from nba_api.stats.endpoints import (
-    commonallplayers,
-    playergamelog
+    commonallplayers
 )
 
-import requests
-from requests.exceptions import ReadTimeout
-
 import pandas as pd
-
 from datetime import datetime
-from time import sleep
 
 from nbaflow.utils.log import log_config
-from nbaflow.utils.requests import handle_timeout_errors
+# from nbaflow.utils.requests import handle_timeout_errors
 
 
 # Setting up a logger object
@@ -31,31 +25,71 @@ logger = log_config(logger_name=__file__)
 
 class NBAPlayers():
     """
+    A class for fetching information about NBA players.
+
+    Args:
+        request_timeout (int, optional):
+            The timeout duration for API requests in seconds. Defaults to 30.
+
+    Attributes:
+        request_timeout (int): The timeout duration for API requests.
+
+    Methods:
+        get_players_info(active_players=True):
+            Retrieve information about NBA players.
+
+    Example:
+        >>> nba_data = NBAPlayers()
+        >>> player_info = nba_data.get_players_info(active_players=True)
     """
 
     def __init__(self, request_timeout: int = 30) -> None:
+        """
+        Initialize the NBAPlayers instance.
+
+        Args:
+            request_timeout (int, optional):
+                The timeout duration for API requests in seconds.
+                Defaults to 30.
+        """
+
         self.request_timeout = request_timeout
 
     def get_players_info(self, active_players: bool = True) -> pd.DataFrame:
         """
+        Retrieve information about NBA players.
+
+        Args:
+            active_players (bool, optional):
+                If True, only active players' information will be returned.
+                Defaults to True.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing information about NBA players.
+
+        Note:
+            This method fetches data about NBA players using the
+            'common_all_players' endpoint from the nba_api.
+            It optionally filters the data to include only active players.
+
+        Example:
+            >>> nba_data = NBAPlayers()
+            >>> player_info = nba_data.get_players_info(active_players=True)
         """
 
         # Getting players information
-        df_players_info = commonallplayers.CommonAllPlayers(
+        df_players = commonallplayers.CommonAllPlayers(
             timeout=self.request_timeout
         ).common_all_players.get_data_frame()
 
         # Preparing the DataFrame columns
-        df_players_info.columns = [
-            col.lower().strip() for col in df_players_info.columns
+        df_players.columns = [
+            col.lower().strip() for col in df_players.columns
         ]
 
         # Getting only active players (if applicable)
         if active_players:
             current_year = str(datetime.now().year)
-            df_players_info = df_players_info.query("to_year == @current_year")
-        
-        return df_players_info
+            df_players = df_players.query(f"to_year == {current_year}")
 
-
-
+        return df_players
--- nbaflow/utils/requests.py
+++ nbaflow/utils/requests.py
@@ -27,6 +27,39 @@ def handle_timeout_errors(
     timeout_increase: int = 5
 ):
     """
+    Handle timeout errors when requesting data from nba_api.
+
+    Args:
+        function (function):
+            The function to call for requesting data.
+
+        function_args (dict):
+            A dictionary containing the arguments to be passed to the function.
+
+        max_attempts (int, optional):
+            The maximum number of attempts to make the request. Defaults to 10.
+
+        timesleep (int, optional):
+            The number of seconds to sleep between attempts. Defaults to 3.
+
+        timeout_increase (int, optional):
+            The amount by which to increase the timeout on each attempt.
+            Defaults to 5.
+
+    Returns:
+        Any: The result returned by the provided function.
+
+    Raises:
+        Exception: If an error occurs during the request process.
+
+    Note:
+        This function handles timeout errors (ReadTimeout) that may occur
+        during the request process. It makes multiple attempts (up to
+        'max_attempts') with increasing timeouts.
+
+    Example:
+        >>> data = handle_timeout_errors(request_data, {'param1': 'value1',
+        'param2': 'value2'}, max_attempts=5)
     """
 
     # Looping over a given number of request attempts
@@ -38,21 +71,17 @@ def handle_timeout_errors(
         # Handling timeout errors
         except ReadTimeout:
             logger.warning(f"Timeout error on requesting data w/ function "
-                        f"{function.__name__}() with the following args "
-                        f"{function_args}. A new attempt will be made with "
-                        f"+{timeout_increase} secs of timeout increase.")
-            
+                           f"{function.__name__}() with the following args "
+                           f"{function_args}. A new attempt will be made with "
+                           f"+{timeout_increase} secs of timeout increase.")
+
             # Increasing timeout on function args for a new attempt
             function_args["timeout"] += timeout_increase
             sleep(timesleep)
-        
+
         # Another error was thrown and so there is nothing to do
         except Exception as e:
             logger.error("Error on trying to request data with function "
-                        f"{function.__name__}() with the following args "
-                        f"{function_args}.")
+                         f"{function.__name__}() with the following args "
+                         f"{function_args}.")
             raise e
-
-
-
-
